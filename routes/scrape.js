15e0fd2437b0459c05c0880d51669773a11555eb
Mars Kim <marsinearth@gmail.com>
2017-06-26 00:23:43 +0000

    scraping stage 1 complete!

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,37 +1,65 @@
-var express = require('express');
-const fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Nightmare = require('nightmare');
+const vo = require('vo');
+const Xvfb = require('xvfb');
+const xvfb = new Xvfb();
+const redis = require('redis'),
+      client = redis.createClient();
+const jsesc = require('jsesc');
 
-router.get('/', function(req, res, next){
-  //sample page
-  const url = 'https://www.ecmrecords.com/catalogue/1487251170/small-town-bill-frisell-thomas-morgan';
-  request(url, function(error, response, html){
-    if(!error){
-      var $ = cheerio.load(html);
-      var title, release, rating;
-      var json = { title: "", release: "", rating: "" };
-      $('.title_wrapper').filter(function(){
-	var data = $(this);
-	title = data.children().first().contents().filter(function(){
-	  return this.nodeType == 3;
-	}).text().trim();
-	json.title = title;
-      });
-      $('#titleYear').filter(function(){
-	var data = $(this);
-	release = data.text().replace(/\(|\)/g, '');
-	json.release = release;
-      });
-      $('.ratingValue').filter(function(){
-	var data = $(this);
-	rating = data.children().first().text();
-	json.rating = rating;
-      });
-      res.json(json);
-    } 
-  });  
-});
+//nightmare scarping function of ECM pages
+function *run(){ 
+  var nightmare = new Nightmare({ 
+    width: 1280,
+    height: 700,
+    show: false
+  });
+
+  //h3.mid
+  return yield nightmare
+    .goto('https://www.ecmrecords.com/catalogue/1484665725')
+    .wait('#mainpage .product .tracklisting')
+    .evaluate(() => {
+      let doc = document, release, rMod, tracks;      
+      let albumObj = { title: "", contents: { artists: "", release: "", album_jacket: "", description: "" }};
+      albumObj.title = doc.querySelector('#mainpage .product .top .images img').title;
+      albumObj.contents.album_jacket = doc.querySelector('#mainpage .product .top .images img').src;
+      albumObj.contents.artists = doc.querySelector('#mainpage .product .container .content .row .col-md-6 h2').textContent;
+      release = doc.querySelector('#mainpage .product .container .content .row .col-md-6 .ps').innerHTML;
+      rMod = release.substring(14, release.indexOf('<br'));
+      albumObj.contents.release = rMod.substr(3,2)+"."+rMod.substr(0,2)+"."+rMod.substr(6,4);      
+      albumObj.contents.description = doc.querySelector('#mainpage .product .press-item p').textContent;
+      tracks = doc.querySelectorAll('#mainpage .product .container .row .tracklisting .playable');
+      for(let i = 0; i < tracks.length; i++) {
+	albumObj.contents["sample_track" + String([i]) + ":title"] = tracks[i].getAttribute('data-title');
+	albumObj.contents["sample_track" + String([i]) + ":movement"] = tracks[i].getAttribute('data-movement');
+	albumObj.contents["sample_track" + String([i]) + ":artists"] = tracks[i].getAttribute('data-artist');
+	albumObj.contents["sample_track" + String([i]) + ":composer"] = tracks[i].getAttribute('data-composer');
+	albumObj.contents["sample_track" + String([i]) + ":URL"] = tracks[i].getAttribute('data-track');
+      }
+      return albumObj;
+    })
+    .end();    
+}
+
+router.get('/', (req, res) => {
+
+  client.on("error", (err) => {
+    console.log("Redis error", err);
+  });
+
+ xvfb.start(() => {
+   vo(run)((err, result) => {
+     if(err) throw err;
+     xvfb.stop();
+     client.hmset("aTitle:" + result.title, result.contents);
+     client.hgetall("aTitle:" + result.title, (err2, reply) => {
+       if(err2) throw err2; 
+       res.json({ title: result.title, contents: reply })       
+     });
+   });   
+ });  
+}); 
 
 module.exports = router;
